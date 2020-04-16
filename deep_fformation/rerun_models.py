5783e3b0c309e6225164eea8d94ee93d8c00e081
Abhijit Gupta <avgupta456@gmail.com>
2020-04-15 21:57:02 -0400

    testing on original codebase

--- deep_fformation/rerun_models.py
+++ deep_fformation/rerun_models.py
@@ -1,16 +1,20 @@
 import numpy as np
+import tensorflow as tf
 
 from utils import load_data, train_and_save_model
 
-global_filters = [64, 128]
-individual_filters = [16, 64]
-combined_filters = [512, 256]
-reg= 1e-07
-dropout= 0.1
+global_filters = [32, 256]
+individual_filters = [16]
+combined_filters = [512, 128, 64]
+reg = 7.943282347242822e-05
+dropout = 0.07
 
 epochs = 600
 dataset = "cocktail_party"
-fold = 0
+fold = 1
+
+tf.set_random_seed(0)
+np.random.seed(0)
 
 test, train, val = load_data("../datasets/" + dataset + "/fold_" + str(0))
 train_and_save_model(global_filters, individual_filters, combined_filters,
